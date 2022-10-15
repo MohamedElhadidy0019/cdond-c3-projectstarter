2a99d940a3375bd291d7061209c4c66c2fa4c47d
Mohamed Khaled <mohamed.elbassiony00@eng-st.cu.edu.eg>
2022-10-16 01:01:28 +0200

    gg

--- .circleci/files/temp.bash
+++ .circleci/files/temp.bash
@@ -1,7 +1,7 @@
 aws cloudformation deploy \
          --template-file cloudfront.yml \
          --stack-name InitialStack \
-         --parameter-overrides WorkflowID=udapeople-kk1j287dhjppmz4370019 \
-         --profile honk \
+         --parameter-overrides WorkflowID=udapeople-kk1j287dhjppmz437998 \
+         --profile kamola \
          --region us-east-1
          #--profile uda_people \
